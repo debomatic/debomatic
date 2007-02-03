02c1d102fb6ac1edc3305128cd2ddf87b0c3b810
Luca Falavigna <dktrkranz@gmail.com>
2007-02-03 14:27:39 +0100

    Initial revision

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Written by Luca Falavigna
+# (C) 2007, Luca Falavigna
+
+from distutils.core import setup
+   
+setup(name='debomatic',
+      version="0.1",
+      scripts=['debomatic']
+)
