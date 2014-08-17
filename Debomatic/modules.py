34498bfccf8340021cf58f20dfc6bb85e5c5616b
Luca Falavigna <dktrkranz@debian.org>
2014-08-17 14:06:54 +0200

    Refactorize module framework to be more solid with dependencies

--- Debomatic/modules.py
+++ Debomatic/modules.py
@@ -21,7 +21,9 @@
 import os
 from glob import glob
 from logging import debug
+from re import findall
 from sys import path
+from toposort import toposort_flatten as toposort
 
 from .process import ModulePool
 
@@ -29,7 +31,6 @@ from .process import ModulePool
 class Module():
 
     def __init__(self, opts):
-        blist = []
         (self._opts, self._rtopts, self._conffile) = opts
         self._use_modules = False
         if self._opts.has_option('modules', 'modules'):
@@ -50,56 +51,38 @@ class Module():
         if self._use_modules:
             self._instances = {}
             self._rtopts.read(self._conffile)
-            if self._rtopts.has_option('runtime', 'modulesblacklist'):
-                blist = self._rtopts.get('runtime', 'modulesblacklist').split()
             for module in modules:
                 try:
                     _class = 'DebomaticModule_%s' % module
                     _mod = __import__(module)
                     self._instances[module] = getattr(_mod, _class)()
-                    self._instances[module]._enabled = True
-                    self._instances[module]._depends = set()
-                    try:
-                        deps = getattr(self._instances[module], 'dependencies')
-                        if deps:
-                            for dep in deps:
-                                self._instances[module]._depends.add(dep)
-                    except AttributeError:
-                        pass
-                    self._instances[module]._enhances = set()
-                    self._instances[module]._missing = set()
                     self._instances[module]._modulename = module
+                    self._instances[module]._disabled = False
+                    self._instances[module]._depends = set()
+                    self._instances[module]._after = set()
                     debug(_('Module %s loaded') % module)
                 except (NameError, SyntaxError):
                     pass
-            for instance in self._instances:
-                instance = self._instances[instance]
-                module = instance._modulename
-                for dep in instance._depends:
-                    if dep in self._instances:
-                        self._instances[dep]._enhances.add(module)
-                if module in blist:
-                    instance._enabled = False
-                    debug(_('Module %s is blacklisted') % module)
-            for module in self._instances:
-                self._check_dependencies(module)
+            self._set_relationships()
+            self._set_blacklisted()
+            self._disable_modules()
+            debug(_('Modules will be executed in this order: %s') %
+                  ', '.join([m for m in self._sort_modules()
+                               if not self._instances[m]._disabled]))
 
-    def _check_dependencies(self, module):
-        instance = self._instances[module]
-        for dep in self._instances[module]._depends:
-            if dep in self._instances:
-                if not self._instances[dep]._enabled:
-                    instance._enabled = False
-                    instance._missing.add(dep)
-            else:
-                instance._enabled = False
-                instance._missing.add(dep)
-        if not instance._enabled:
-            if instance._missing:
+    def _disable_modules(self):
+        for module in self._sort_modules():
+            missing = set()
+            for dep in self._instances[module]._depends:
+                if dep in self._instances:
+                    if self._instances[dep]._disabled:
+                        missing.add(dep)
+                else:
+                    missing.add(dep)
+            if missing:
+                self._instances[module]._disabled = True
                 debug(_('%s module disabled, needs %s') %
-                      (instance._modulename, ', '.join(instance._missing)))
-            for module in instance._enhances:
-                self._check_dependencies(module)
+                      (module, ', '.join(missing)))
 
     def _launcher(self, hook):
         func, args, module, hookname, dependencies = hook
@@ -107,14 +90,69 @@ class Module():
               {'hook': hookname, 'mod': module})
         func(args)
 
+    def _set_blacklisted(self):
+        blist = set()
+        if self._rtopts.has_option('runtime', 'modulesblacklist'):
+            _blacklist = self._rtopts.get('runtime', 'modulesblacklist')
+            blist = set(_blacklist.split())
+        for module in self._instances:
+            if module in blist:
+                self._instances[module]._disabled = True
+                debug(_('Module %s is blacklisted') % module)
+
+    def _set_relationships(self):
+        for module in self._instances:
+            try:
+                deps = getattr(self._instances[module], 'dependencies')
+                if deps:
+                    for dep in deps:
+                        self._instances[module]._depends.add(dep)
+            except AttributeError:
+                pass
+            try:
+                afters = getattr(self._instances[module], 'after')
+                if afters:
+                    for after in afters:
+                        self._instances[module]._after.add(after)
+            except AttributeError:
+                pass
+            try:
+                befores = getattr(self._instances[module], 'before')
+                if befores:
+                    for before in befores:
+                        if before in self._instances:
+                            self._instances[before]._after.add(module)
+            except AttributeError:
+                pass
+
+    def _sort_modules(self):
+        modules = {}
+        for instance in self._instances:
+            if not self._instances[instance]._disabled:
+                _deps = self._instances[instance]._depends
+                _afters = self._instances[instance]._after
+                modules[instance] = _deps.union(_afters)
+        try:
+            return [m for m in toposort(modules) if m in self._instances]
+        except ValueError as e:
+            circular = findall('.*?\(\'?(\S+?)\'?,', e.args[0])
+            for instance in circular:
+                self._instances[instance]._disabled = True
+            debug(_('Circular dependencies found, disabled modules: %s')
+                  % ', '.join(circular))
+            return self._sort_modules()
+
     def execute_hook(self, hook, args):
         hooks = []
         if self._use_modules:
-            for instance in [self._instances[m]
-                             for m in sorted(self._instances)
-                             if self._instances[m]._enabled]:
-                module = instance._modulename
-                dependencies = instance._depends
+            for module in self._sort_modules():
+                if self._instances[module]._disabled:
+                    continue
+                dependencies = set()
+                instance = self._instances[module]
+                for dep in instance._depends.union(instance._after):
+                    if dep in self._instances:
+                        dependencies.add(dep)
                 try:
                     hooks.append((getattr(instance, hook),
                                  args, module, hook, dependencies))
