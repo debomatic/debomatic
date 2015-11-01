fe2f5248d65f9b53bcb5900ab2ae6dc0017d08c5
Luca Falavigna <dktrkranz@debian.org>
2015-11-01 13:45:48 +0100

    Try to update local repository atomically

--- modules/Repository.py
+++ modules/Repository.py
@@ -21,8 +21,9 @@
 
 import os
 from datetime import datetime
+from fcntl import flock, LOCK_EX, LOCK_UN
 from subprocess import call, PIPE
-from time import sleep
+from tempfile import NamedTemporaryFile
 
 
 class DebomaticModule_Repository:
@@ -42,6 +43,21 @@ class DebomaticModule_Repository:
         if args.action:
             self.update_repository(args)
 
+    class Lock:
+
+        def __init__(self, distribution, architecture):
+            self._file = ('/var/run/debomatic-%s-%s.apt.lock' %
+                          (distribution, architecture))
+
+        def __enter__(self):
+            self._fd = open(self._file, 'w')
+            flock(self._fd, LOCK_EX)
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            flock(self._fd, LOCK_UN)
+            self._fd.close()
+
     def update_repository(self, args):
         if args.opts.has_section('repository'):
             gpgkey = args.opts.get('repository', 'gpgkey')
@@ -59,56 +75,65 @@ class DebomaticModule_Repository:
             arch = args.hostarchitecture
         else:
             arch = args.architecture
-        if not os.path.exists(self.lockdir):
-            os.makedirs(self.lockdir)
-        lockfile = os.path.join(self.lockdir, '%s.%s.apt.lock' %
-                                (distribution, arch))
-        while True:
-            if not os.listdir(self.lockdir):
-                open(lockfile, 'w+').close()
-                break
-            sleep(5)
         archive = args.directory
         pool = os.path.join(archive, 'pool')
         dists = os.path.join(archive, 'dists', distribution)
         packages = os.path.join(dists, 'main', 'binary-%s' % arch)
         packages_file = os.path.join(packages, 'Packages')
+        packages_file_tmp = os.devnull
         arch_release_file = os.path.join(packages, 'Release')
+        arch_release_file_tmp = os.devnull
         release_file = os.path.join(dists, 'Release')
+        release_file_tmp = os.devnull
         release_gpg = os.path.join(dists, 'Release.gpg')
+        release_gpg_tmp = os.devnull
         inrelease_gpg = os.path.join(dists, 'InRelease')
+        inrelease_gpg_tmp = os.devnull
         for dir in (pool, dists, packages):
             if not os.path.isdir(dir):
                 os.makedirs(dir)
         os.chdir(archive)
-        with open(packages_file, 'w') as fd:
-            call([self.af, 'packages', 'pool'], stdout=fd, stderr=PIPE)
-        with open(arch_release_file, 'w') as fd:
-            fd.write('Origin: Deb-O-Matic\n')
-            fd.write('Label: Deb-O-Matic\n')
-            fd.write('Archive: %s\n' % distribution)
-            fd.write('Component: main\n')
-            fd.write('Architecture: %s\n' % arch)
-        with open(release_file, 'w') as fd:
-            date = datetime.now().strftime('%A, %d %B %Y %H:%M:%S')
-            call([self.af, '-qq',
-                  '-o', 'APT::FTPArchive::Release::Origin=Deb-o-Matic',
-                  '-o', 'APT::FTPArchive::Release::Label=Deb-o-Matic',
-                  '-o', 'APT::FTPArchive::Release::Suite=%s' % distribution,
-                  '-o', 'APT::FTPArchive::Release::Date=%s' % date,
-                  '-o', 'APT::FTPArchive::Release::Architectures=%s' % arch,
-                  '-o', 'APT::FTPArchive::Release::Components=main',
-                  'release', 'dists/%s' % distribution],
-                 stdout=fd, stderr=PIPE)
-        with open(release_file, 'r+') as fd:
-            data = fd.read()
-            fd.seek(0)
-            fd.write(data.replace('MD5Sum', 'NotAutomatic: yes\nMD5Sum'))
-        call([self.gpg, '--no-default-keyring', '--keyring', pubring,
-              '--secret-keyring', secring, '-u', gpgkey, '--yes', '-a',
-              '-o', release_gpg, '-s', release_file])
-        call([self.gpg, '--no-default-keyring', '--keyring', pubring,
-              '--secret-keyring', secring, '-u', gpgkey, '--yes', '-a',
-              '-o', inrelease_gpg, '--clearsign', release_file])
-        os.unlink(lockfile)
+        with self.Lock(distribution, arch):
+            with NamedTemporaryFile('w', dir=packages, delete=False) as fd:
+                call([self.af, 'packages', 'pool'], stdout=fd, stderr=PIPE)
+                packages_file_tmp = fd.name
+            with NamedTemporaryFile('w', dir=packages, delete=False) as fd:
+                fd.write('Origin: Deb-O-Matic\n')
+                fd.write('Label: Deb-O-Matic\n')
+                fd.write('Archive: %s\n' % distribution)
+                fd.write('Component: main\n')
+                fd.write('Architecture: %s\n' % arch)
+                arch_release_file_tmp = fd.name
+            with NamedTemporaryFile('w', dir=packages, delete=False) as fd:
+                date = datetime.now().strftime('%A, %d %B %Y %H:%M:%S')
+                afstring = 'APT::FTPArchive::Release::'
+                call([self.af, '-qq',
+                      '-o', '%sOrigin=Deb-o-Matic' % afstring,
+                      '-o', '%sLabel=Deb-o-Matic' % afstring,
+                      '-o', '%sSuite=%s' % (afstring, distribution),
+                      '-o', '%sDate=%s' % (afstring, date),
+                      '-o', '%sArchitectures=%s' % (afstring, arch),
+                      '-o', '%sComponents=main' % afstring,
+                      'release', 'dists/%s' % distribution],
+                     stdout=fd, stderr=PIPE)
+                release_file_tmp = fd.name
+            with open(release_file_tmp, 'r+') as fd:
+                data = fd.read()
+                fd.seek(0)
+                fd.write(data.replace('MD5Sum', 'NotAutomatic: yes\nMD5Sum'))
+            with NamedTemporaryFile('w', dir=packages, delete=False) as fd:
+                call([self.gpg, '--no-default-keyring', '--keyring', pubring,
+                      '--secret-keyring', secring, '-u', gpgkey, '--yes',
+                      '-a', '-o', fd.name, '-s', release_file_tmp])
+                release_gpg_tmp = fd.name
+            with NamedTemporaryFile('w', dir=packages, delete=False) as fd:
+                call([self.gpg, '--no-default-keyring', '--keyring', pubring,
+                      '--secret-keyring', secring, '-u', gpgkey, '--yes',
+                      '-a', '-o', fd.name, '--clearsign', release_file])
+                inrelease_gpg_tmp = fd.name
+            os.rename(packages_file_tmp, packages_file)
+            os.rename(arch_release_file_tmp, arch_release_file)
+            os.rename(release_file_tmp, release_file)
+            os.rename(release_gpg_tmp, release_gpg)
+            os.rename(inrelease_gpg_tmp, inrelease_gpg)
         os.chdir(cwd)
