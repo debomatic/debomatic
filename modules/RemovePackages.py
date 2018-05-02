e9f9c950141596e9e64577ad3af4364fdbc13b8e
Luca Falavigna <dktrkranz@debian.org>
2018-05-02 16:42:41 +0200

    Allow removal of empty suite folders

--- modules/RemovePackages.py
+++ modules/RemovePackages.py
@@ -34,8 +34,9 @@ class DebomaticModule_RemovePackages:
         if args.opts.has_section('removepackages'):
             delta = args.opts.getint('removepackages', 'days') * 24 * 60 * 60
             if os.path.isdir(args.directory):
-                for arch in os.listdir(args.directory):
-                    pool = os.path.join(args.directory, arch, 'pool')
+                for suite in os.listdir(args.directory):
+                    element = os.path.join(args.directory, suite)
+                    pool = os.path.join(element, 'pool')
                     if os.path.isdir(pool):
                         for package in os.listdir(pool):
                             package = os.path.join(pool, package)
@@ -43,3 +44,9 @@ class DebomaticModule_RemovePackages:
                                 ptime = os.stat(package).st_mtime
                                 if ptime + delta < ctime:
                                     rmtree(package)
+                        if not os.listdir(pool):
+                            rmtree(pool)
+                    if os.path.isdir(element):
+                        if not [f for f in os.listdir(element)
+                                if f != 'dists' and f != 'logs']:
+                            rmtree(element)
