4dc7de0f02fdae5b4c40862834d77047b1589d40
Luca Falavigna <dktrkranz@debian.org>
2022-07-10 13:52:42 +0200

    Stable release 0.26

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ class InstallData(install_data):
 
 
 setup(name='debomatic',
-      version='0.25',
+      version='0.26',
       author='Luca Falavigna',
       author_email='dktrkranz@debian.org',
       description='Automatic build machine for Debian source packages',
